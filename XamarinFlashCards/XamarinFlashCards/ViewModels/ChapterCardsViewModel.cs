eb2ad38e35b8564e96428378630caa263a5564a0
maryanne-exe <maryannexe@ya.ru>
2017-08-06 01:19:44 -0400

    Create ChapterCards Page & ViewModel

---
+++ XamarinFlashCards/XamarinFlashCards/ViewModels/ChapterCardsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamarinFlashCards
+{
+    public class ChapterCardsViewModel : BaseViewModel
+    {
+        public ChapterCardsViewModel(Chapter chapter)
+        {
+            Title = "Cards";
+        }
+    }
+}
+
---
+++ XamarinFlashCards/XamarinFlashCards/Views/ChapterCardsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFlashCards
+{
+    public partial class ChapterCardsPage : ContentPage
+    {
+        public ChapterCardsPage(ChapterCardsViewModel model)
+        {
+            InitializeComponent();
+
+            BindingContext = model;
+        }
+    }
+}
